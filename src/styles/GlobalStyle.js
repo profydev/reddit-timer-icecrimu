bc42db5716197011bcd186e3e0b0973881eaa084
icecrimu <charles.banigon@gmail.com>
2022-04-11 00:26:42 +0800

    Set text-decoration none to remove underline

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -9,8 +9,16 @@ const GlobalStyle = createGlobalStyle`
         font-family: ${({ theme }) => theme.font.family.default};
         font-size: ${({ theme }) => theme.font.size.default};
         line-height: ${({ theme }) => theme.font.lineHeight.default};
-        color: ${({ theme }) => theme.color.text}
+        color: ${({ theme }) => theme.color.text};
+        
     }
+
+    a {
+        text-decoration: none;
+
+    }
+
+
 `;
 
 export default GlobalStyle;
